f7cef315d177a82f0f2e78315f5e6e029a8cc119
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-08-06 23:55:34 +0530

    Login Page Done

--- src/pages/logInPage/LogIn.jsx
+++ src/pages/logInPage/LogIn.jsx
@@ -2,21 +2,40 @@ import {Component} from "react";
 import {withStyles} from "@mui/styles";
 import {styleSheet} from "./Styles";
 import {ValidatorForm} from "react-material-ui-form-validator";
-import {Grid} from "@mui/material";
+import {Button, Grid, TextField, Typography} from "@mui/material";
 
-class LogIn extends Component{
+class LogIn extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
         const {classes} = this.props
-        return(
+        return (
             <>
                 <ValidatorForm ref="form" className="pt-2">
 
-                    <Grid className={classes.logIn_container} >
-
+                    <Grid className={classes.logIn_container}>
+                        <Grid className={classes.logIn_form}>
+                            <Grid className={classes.logIn_header}>
+                                <Typography variant="h3" gutterBottom component="div" >
+                                    Login
+                                </Typography>
+                            </Grid>
+                            <Grid className={classes.text_field}>
+                                <TextField id="outlined-basic" label="User Name" variant="outlined" size="small"/>
+                                <TextField id="outlined-basic" label="Password" type="password" variant="outlined"
+                                           size="small"/>
+                            </Grid>
+                            <Grid className={classes.logIn_button}>
+                                <Button variant="contained" style={{backgroundColor:'#7C8DEA',color:'black',width:'150px'}}>Login</Button>
+                            </Grid>
+                            <Grid className={classes.click_here}>
+                                <Typography variant="body2" gutterBottom component="div" style={{color:'black'}}>
+                                    Create new user account? <span style={{color:'blue'}}>click here</span>
+                                </Typography>
+                            </Grid>
+                        </Grid>
                     </Grid>
 
                 </ValidatorForm>
--- src/pages/logInPage/Styles.js
+++ src/pages/logInPage/Styles.js
@@ -8,6 +8,59 @@ export const styleSheet= {
         display: 'flex',
         flexWrap: 'wrap',
         flexDirection: 'column',
-        backgroundColor:'#5B6C8C'
+        backgroundColor:'#D9D9D9'
+    },
+
+    logIn_form:{
+        width: "424px",
+        height:"421px",
+        alignItems: 'center',
+        justifyContent: 'center',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        backgroundColor: "#C5B5B5"
+    },
+
+    logIn_header:{
+        width: "424px",
+        height:"100px",
+        alignItems: 'center',
+        justifyContent: 'center',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        color: "#ffffff"
+    },
+
+    text_field:{
+        width: "424px",
+        height:"150px",
+        alignItems: 'center',
+        justifyContent: 'center',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+    },
+
+    logIn_button:{
+        width: "424px",
+        height:"71px",
+        alignItems: 'center',
+        justifyContent: 'center',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+    },
+
+    click_here:{
+        width: "424px",
+        height:"100px",
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        margin:'0 0 20px 30px'
     },
 }
